d6b3d09d25ed551bda52aa7354de8ddecd45b28e
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2021-12-02 16:53:24 -0800

    Fixed flight

--- Frontend/src/components/About/About.js
+++ Frontend/src/components/About/About.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 class About extends Component {
   render() {
     return (
-      <div >
-        <h1 className='headerh1'>About</h1>
+      <div>
+        <h1 className="headerh1">About</h1>
       </div>
     );
   }
